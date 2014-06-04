e98a8c2fcfabd9eb763f6115d5bf6a5329f5564c
Ryan Cornelsen <cornelsen@gmail.com>
2014-06-03 18:15:19 -0700

    Moved configuration options to a separate INI file for easier maintenance and to make their usage more intuitive

--- jenkins.py
+++ jenkins.py
@@ -1,35 +1,29 @@
+import ConfigParser
 import jobstatus
+import json
 import sys
 import time
 
 settings = ConfigParser.ConfigParser()
 settings.read(['jenkins.ini'])
 
-JENKINS_BASE_URL = 'https://my.jenkins-ci.org'
-JENKINS_JSON_URL = JENKINS_BASE_URL + '/job/%s/lastBuild/api/json'
-JENKINS_USERNAME = 'authenticated_user'
-JENKINS_PASSWORD = 'authenticated_password'
-JENKINS_JOB_LIST = ['job1', 'job2']
+JENKINS_USERNAME = settings.get('Global', 'USERNAME')
+JENKINS_PASSWORD = settings.get('Global', 'PASSWORD')
+QUERY_INTERVAL_SEC = int(settings.get('Global', 'QUERY_INTERVAL_SEC'))
+JENKINS_JOB_LIST = []
 
-passJobs = []
-warnJobs = []
-failJobs = []
-initJobs = []
-ledStatus = LedStatus()
+for section in settings.sections():
+    if 'Global' != section:
+        base_url = settings.get(section, 'BASE_URL')
+        json_url = settings.get(section, 'JSON_URL')
+        job_list = json.loads(settings.get(section, 'JOB_LIST'))
+        for job in job_list:
+            JENKINS_JOB_LIST.append(base_url + json_url % job)
 
 
 def main():
-    global passJobs
-    global warnJobs
-    global failJobs
-    global initJobs
     job_status = jobstatus.JobStatus(JENKINS_USERNAME, JENKINS_PASSWORD, JENKINS_JOB_LIST)
     while True:
-        passJobs = []
-        warnJobs = []
-        failJobs = []
-        initJobs = []
-
         job_status.run()
         time.sleep(QUERY_INTERVAL_SEC)
 
